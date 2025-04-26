4eb002531d03ce1fe27c59aa07ceb7cb24dc4e40
Roy Wei <roywjy@gmail.com>
2025-04-26 15:48:37 +1000

    Enhance DoublyLinkedList class with node type definition and implement get method for retrieving values by index

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,27 +1,46 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+    next?: Node<T>;
+}
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.tail = undefined;
+        this.length = 0;
     }
 
     prepend(item: T): void {
 
-}
+    }
+    
     insertAt(item: T, idx: number): void {
 
-}
+    }
+
     append(item: T): void {
 
-}
+    }
+
     remove(item: T): T | undefined {
 
-}
-    get(idx: number): T | undefined {
+    }
 
-}
+    get(idx: number): T | undefined {
+        let curr = this.head;
+        // loop through the list until we find the index, and it has to be something there
+        for (let i = 0; i < idx && curr; i++) {
+            curr = curr.next;
+        }
+        return curr?.value;
+    }
+
     removeAt(idx: number): T | undefined {
 
-}
+    }
 }
