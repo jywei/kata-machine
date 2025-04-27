474080fb4c63830c2d362d4ae4700264d2ed6366
Roy Wei <roywjy@gmail.com>
2025-04-27 16:23:55 +1000

    Refactor QuickSort implementation to use recursive approach with partitioning logic

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,37 @@
-export default function quick_sort(arr: number[]): void {
+function quick_sort_recur(arr: number[], low: number, high: number): void {
+    if (low >= high) {
+        return;
+    }
 
+    const pivotIdx = partition(arr, low, high);
+
+    // left
+    quick_sort_recur(arr, low, pivotIdx - 1);
+    // right
+    quick_sort_recur(arr, pivotIdx + 1, high);
+}
+
+function partition(arr: number[], low: number, high: number): number {
+    const pivot = arr[high];
+    let idx = low - 1;
+
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    idx++;
+    const tmp = arr[idx];
+    arr[idx] = arr[high];
+    arr[high] = tmp;
+
+    return idx;
+}
+
+export default function quick_sort(arr: number[]): void {
+    quick_sort_recur(arr, 0, arr.length - 1);
 }
